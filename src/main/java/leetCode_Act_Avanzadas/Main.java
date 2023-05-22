481b050e63c41601f4bdce1dcb55eda6ccbb71cf
KevinSarmiento07 <kevinandressasi@ufps.edu.co>
2023-05-22 18:46:15 -0500

    Subiendo mas ejercicios

---
+++ src/main/java/leetCode_Act_Avanzadas/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetCode_Act_Avanzadas;
+
+import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
+ 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+    	Set<Integer> color = new HashSet<>();
+        int[] numArray = new int[N];
+    	for(int i = 0; i < N; i++) {
+    		numArray[i] = sc.nextInt();
+    		color.add(numArray[i]);
+    	}
+    	System.out.println(color.size());
+    }
+}
